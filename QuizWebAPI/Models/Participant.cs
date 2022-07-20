b1de33ed36188eda3385ad71c8321c243c9b3418
berniemackie97 <berniemack97@icloud.com>
2022-07-19 22:33:34 -0400

    Add project files.

---
+++ QuizWebAPI/Models/Participant.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuizWebAPI.Models
+{
+    public class Participant
+    {
+        [Key]
+        public int ParticipantId { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Email { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Name { get; set; }
+
+        public int Score { get; set; }
+
+        public int TimeTaken { get; set; }
+    }
+
+    public class ParticipantResult
+    {
+        public int ParticipantId { get; set; }
+        public int Score { get; set; }
+        public int TimeTaken { get; set; }
+    }
+}
---
+++ QuizWebAPI/Models/QuizDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace QuizWebAPI.Models
+{
+    public class QuizDBContext : DbContext
+    {
+        public QuizDBContext(DbContextOptions<QuizDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+    }
+}
